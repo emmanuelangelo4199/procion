a6f1b5a1d4ef852679e6e96a79eb3532ee25d2f0
emmanuelangelo4199 <emmanuelmezie578@gmail.com>
2026-06-04 14:18:05 +0000

    merge: bring dev templates into main

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def landing_page(request):
+
+    context = {}
+
+    return render(request, "app/landing_page.html", context)
+
+def dashboard(request):
+
+    context = {}
+    return render(request, "app/dashboard.html", context)
