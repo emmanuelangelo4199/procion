a1a7b51e4dff06dd4cd7309b9d0f1e9f0d502d01
emmanuelangelo4199 <emmanuelmezie578@gmail.com>
2026-06-06 23:52:22 +0000

    feat: urls mapping

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', landing_page, name='home'),
+    path('login', login_page, name='login'),
+    path('signup', signup_page, name='signup'),
+    path('dashboard', dashboard, name='dashboard'),
+    path('journal', emotion_food_logger, name='journal'),
+    path('insights', progress_insight, name='insights'),
+    path('circle', my_circle, name='circle'),
+    path('profile', profile_settings, name='profile'),
+]
