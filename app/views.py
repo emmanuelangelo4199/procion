a599cffe4963503ddf7fe41d6ebeb1ffa7b6f809
emmanuelangelo4199 <emmanuelmezie578@gmail.com>
2026-06-06 23:55:08 +0000

    feat: Add placeholder views for core Porcion pages

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 
@@ -8,7 +8,37 @@ def landing_page(request):
 
     return render(request, "app/landing_page.html", context)
 
+def login_page(request):
+
+    context = {}
+    return render(request, "app/login_page.html", context)
+
+def signup_page(request):
+
+    context = {}
+    return render(request, "app/signup_page.html", context)
+
 def dashboard(request):
 
     context = {}
     return render(request, "app/dashboard.html", context)
+
+def emotion_food_logger(request):
+
+    context = {}
+    return render(request, "app/emotion_food_logger.html", context)
+
+def progress_insight(request):
+
+    context = {}
+    return render(request, "app/progress_insight.html", context)
+
+def my_circle(request):
+
+    context = {}
+    return render(request, "app/my_circle.html", context)
+
+def profile_settings(request):
+
+    context = {}
+    return render(request, "app/profile_settings.html", context)
