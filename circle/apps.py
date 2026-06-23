65d8ed2af7465d295c022d78ee3afdaae364d0ab
emmanuelangelo4199 <emmanuelmezie578@gmail.com>
2026-06-23 10:11:29 +0000

    feat(circle): add signals files when django starts up [the signals.py file enforces the user size limit and throws an exception when the limit has been meet. and also raises a value exception it meets the minimum size].

--- circle/apps.py
+++ circle/apps.py
@@ -2,4 +2,10 @@ from django.apps import AppConfig
 
 
 class CircleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'circle'
+
+    def ready(self):
+        # This imports the signals file when Django starts up
+        import circle.signals
+ 
