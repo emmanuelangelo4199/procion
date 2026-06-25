1a8720f31210a656b1ecc7374e285404fcf4109b
emmanuelangelo4199 <emmanuelmezie578@gmail.com>
2026-06-25 21:37:06 +0000

    feat (circle): defined a leave_circle function - implemented django first query set - add exception handling

--- circle/views.py
+++ circle/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from .models import Circle
 
 
+@login_required 
 def my_circle(request):
+    circle = request.user.circles.first()
 
-    context = {}
+    context = {'circle': circle}
     return render(request, "circle/my_circle.html", context)
+
+@login_required
+def leave_circle(request):
+    circle = request.user.circles.first()
+    if circle is None:
+        messages.error(request, "You're not currently in a circle.")
+        return redirect('circle')
+
+    if request.method == "POST":
+        try:
+            circle.remove_member(request.user)
+            messages.success(request, "You've left the circle.")
+        except ValidationError as e:
+            messages.error(request, " ".join(e.messages))
+    return redirect('circle')
