ca59b61aa9198ec49274e310c23bc406d52ece13
emmanuelangelo4199 <emmanuelmezie578@gmail.com>
2026-07-01 23:02:56 +0000

    chore(admin): register circle-related models in Django admin

--- circle/admin.py
+++ circle/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Circle, Message, WeeklyPrompt, PromptAnswer, CircleWaitlist
 
-# Register your models here.
+admin.site.register(Circle)
+admin.site.register(Message)
+admin.site.register(WeeklyPrompt)
+admin.site.register(PromptAnswer)
+admin.site.register(CircleWaitlist)
