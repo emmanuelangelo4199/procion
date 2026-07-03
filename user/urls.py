90a5261290d98f004a2462a8ffc9bc6ed6f7c526
Angelo ●◡● <151058058+emmanuelangelo4199@users.noreply.github.com>
2026-07-03 22:40:38 +0000

    Refactor URL patterns in user/urls.py
    Removed unused URL paths for dashboard, journal, insights, and circle.

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from .views import *
+from .views import login_page, signup_page, profile_settings
 
 urlpatterns = [
     path('login', login_page, name='login'),
     path('signup', signup_page, name='signup'),
-    path('dashboard', dashboard, name='dashboard'),
-    path('journal', emotion_food_logger, name='journal'),
-    path('insights', progress_insight, name='insights'),
-    path('circle', my_circle, name='circle'),
     path('profile', profile_settings, name='profile'),
 ]
 
