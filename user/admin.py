60044b995361d9fadc70282bc0adeeb51a7ba9b3
Angelo ●◡● <151058058+emmanuelangelo4199@users.noreply.github.com>
2026-07-04 18:44:59 +0000

    Enhance admin interface for User and UserProfile models
    Implemented a custom admin interface for User model with tailored fieldsets and display options.

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, UserProfile
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'name', 'is_staff', 'is_active')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('email', 'name', 'password1', 'password2')}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
